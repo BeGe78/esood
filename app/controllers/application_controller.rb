5b96f6d29222cf4fdeaf4782c9a451e1d30b0ec5
root <bgardin@gmail.com>
2016-05-26 10:44:53 +0200

    test de sécurisation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception  
   before_action :set_locale  #activation des langues, en et fr pour cette application
- 
+  
 def set_locale
   I18n.locale = params[:locale] || I18n.default_locale
 #  I18n.locale = :fr 
@@ -12,19 +12,6 @@ end
 def default_url_options(options={})
   { :locale => I18n.locale == I18n.default_locale ? nil : I18n.locale  }
 end
-=begin
-def default_url_options(options = {})
-  { locale: I18n.locale }.merge options
-end
-
-def set_locale
-  logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"   #define local language
-#  I18n.locale = extract_locale_from_accept_language_header
-#  I18n.default_locale = :fr
-  I18n.locale = :fr
-  logger.debug "* Locale set to '#{I18n.locale}'"
-end
-=end
  
 private
   def extract_locale_from_accept_language_header
