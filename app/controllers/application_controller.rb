4acf17e44f90fd36b7e8faf5ae4b8c835fc5f84d
root <bgardin@gmail.com>
2016-05-30 16:16:35 +0200

    Translate devise views and messages in french

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,18 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception  
   before_action :set_locale  #activation des langues, en et fr pour cette application
-  
+    before_action :configure_permitted_parameters, if: :devise_controller?
+protected
+
+def configure_permitted_parameters
+  devise_parameter_sanitizer.permit(:sign_up) do |u|
+     u.permit({ roles: [] }, :email, :password, :password_confirmation, :name)
+  end
+  devise_parameter_sanitizer.permit(:account_update) do |u|
+     u.permit({ roles: [] }, :email, :password, :password_confirmation, :name)
+  end
+end
+
 def set_locale
   I18n.locale = params[:locale] || I18n.default_locale
 #  I18n.locale = :fr 
@@ -18,3 +29,4 @@ private
     request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
   end
 end
+
