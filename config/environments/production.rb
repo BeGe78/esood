031da2e0ee6926820b125d5e8c8f5bbc9d1b9db8
root <bgardin@gmail.com>
2016-05-31 14:41:46 +0200

    Change HOST name and port in production config file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ GA.tracker = "UA-78195871-1"
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-config.action_mailer.default_url_options = { host: 'localhost', port: 80 }
+config.action_mailer.default_url_options = { host: ENV["HOST_NAME"], port: ENV["HOST_PORT"] }
 config.action_mailer.raise_delivery_errors = true
 config.action_mailer.perform_deliveries = true
 config.action_mailer.delivery_method = :smtp
