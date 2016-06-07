78cb2d47a8754d8a69d838181cfa6c1329735f37
root <bgardin@gmail.com>
2016-06-07 20:58:19 +0200

    handle errors in registration: credit card errors or password errors

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -8,20 +8,48 @@ class RegistrationsController < Devise::RegistrationsController
     build_resource(sign_up_params)
     puts(params[:stripeEmail])
     puts(resource.plan_id)
+# initialize empty fields    
+    resource.email = params[:stripeEmail]
+    resource.stripe_card_token = params[:stripeToken]
+    resource.invoice_count = 1   #need to initialize to 1 because customer.created event can arrive after
+    resource.valid?
+    puts(resource.errors.messages)
+    puts(resource.errors.messages.length)
+# check if the user models verifications are good
+    if resource.errors.messages.length > 0    # there is at least an error
+     case resource.errors.keys.first
+      when :password_confirmation
+          redirect_to_back_or_default(alert: t('password_confirmation_error'))
+      when :email
+          redirect_to_back_or_default(alert: t('email_allready_used'))
+      when :password
+          redirect_to_back_or_default(alert: t('password_blanck_error'))
+      when :name
+          redirect_to_back_or_default(alert: t('name_blanck_error'))
+      else 
+          redirect_to_back_or_default(alert: t('user_parameter_error'))
+    end        
 # check that this email has not yet been created in user table
-    if User.exists?(email: params[:stripeEmail])
+    elsif User.exists?(email: params[:stripeEmail])
       redirect_to_back_or_default(alert: t('email_allready_used'))
     else     
-# create Stripe customer first      
+# create Stripe customer first
+      begin  
       customer= Stripe::Customer.create(
       :email => params[:stripeEmail],         
       :source  => params[:stripeToken],
       :plan => resource.plan_id
       )
+      rescue Stripe::CardError => e
+          flash[:notice] = t('stripe_card_error')
+          redirect_to new_selector_path and return
+      rescue Stripe::StripeError => e
+          flash[:notice] = t('stripe_error')
+          redirect_to new_selector_path and return    
+      end 
+      
 # create Devise user    
-      resource.email = params[:stripeEmail]
-      resource.stripe_card_token = params[:stripeToken]
-      resource.invoice_count = 1   #need to initialize to 1 because customer.created event can arrive after
+      
       resource.save
       yield resource if block_given?
       if resource.persisted?
@@ -43,15 +71,25 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def update
+    begin
     cu = Stripe::Customer.retrieve(resource.customer_id)
     cu.email = params[:user][:email]   #we need to update the email in case it has been modified
     cu.save
+    rescue Stripe::StripeError => e
+          flash[:notice] = t('stripe_error')
+          redirect_to edit_user_registration_path and return
+    end
     super
   end
   def destroy
 # first delete Stripe customer
+    begin
     cu = Stripe::Customer.retrieve(resource.customer_id)
     cu.delete
+    rescue Stripe::StripeError => e
+          flash[:notice] = t('stripe_error')
+          redirect_to edit_user_registration_path and return
+    end
 # then delete user with Devise 
     super
   end
