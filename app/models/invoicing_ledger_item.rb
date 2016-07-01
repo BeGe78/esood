013e56e346d9ed2be100806b3f2f30732ce40990
root <bgardin@gmail.com>
2016-07-01 12:22:11 +0200

    correct bug in destroy user + invoices

--- app/models/invoicing_ledger_item.rb
+++ app/models/invoicing_ledger_item.rb
@@ -1,6 +1,6 @@
 class InvoicingLedgerItem < ActiveRecord::Base
   acts_as_ledger_item
-  belongs_to :recipient, class_name: 'User'
-  belongs_to :sender, class_name: 'User'
+  belongs_to :recipient, class_name: 'User', foreign_key: :recipient_id 
+  belongs_to :sender, class_name: 'User', foreign_key: :sender_id 
   has_many :line_items, class_name: 'InvoicingLineItem', foreign_key: :ledger_item_id
 end
